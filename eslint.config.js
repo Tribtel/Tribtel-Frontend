15224cf9593020d0edb7f2f301546aa32396b5fa
Plutoball <likatshabalala@gmail.com>
2026-01-02 15:35:17 +0200

    debug  -eslint trial

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,39 @@
 // eslint.config.js
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import prettier from 'eslint-plugin-prettier';
+import js from "@eslint/js";
+import parser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-plugin-prettier";
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    prettier,
-  },
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react-refresh/only-export-components': 'warn',
-    'prettier/prettier': 'warn',
-  },
-});
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      parser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier,
+    },
+    rules: {
+      // TypeScript rules
+      "@typescript-eslint/no-unused-vars": "warn",
+
+      // React rules
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": "warn",
+
+      // Code style
+      "eqeqeq": "error",
+      "prettier/prettier": "warn",
+    },
+    ignores: ["node_modules", "dist", "build"],
+  }
+);
